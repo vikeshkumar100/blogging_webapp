e1abf4ed3a47309c7418175cd2bcc6010d85b195
Vikesh <vikeshkapoor100@gmail.com>
2026-03-10 15:06:47 +0530

    ejs layouts structure done

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
-import express from 'express';
+import express, { urlencoded } from 'express';
 import dotenv from 'dotenv';
+import path from 'path';
+import expressEjsLayouts from 'express-ejs-layouts';
+
 dotenv.config();
+
 const PORT=process.env.PORT || 8000;
 const app=express();
+
+app.set('view engine','ejs');
+app.set('views',path.resolve('./views'));
+
+app.use(expressEjsLayouts);
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.set("layout","layout");
 
 app.get('/',(req,res)=>{
     res.send("Hi, this is blogging app");
 })
+app.get('/blogs',(rew,res)=>{
+    res.render('login');
+});
 
 app.listen(PORT,()=>{
     console.log("Server is running on port - ",PORT);
