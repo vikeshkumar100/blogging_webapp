ddaaae275aea0c6886149b316134b3b822ae0da6
Vikesh <vikeshkapoor100@gmail.com>
2026-03-21 18:15:58 +0530

    fix: jwt verification in middleware

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -4,5 +4,11 @@ export const verifyLogin=(req,res,next)=>{
     const token=req.cookies.jwt;
     if(!token)
         return res.redirect('/auth/login');
-    next();
+    try{
+        jwt.verify(token,"vikesh");
+        next();
+    }catch(err)
+    {
+        return res.redirect('/auth/login');
+    }
 }
